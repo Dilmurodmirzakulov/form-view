acbe9a090362a65b61e5106f82bd12053ee078d8
Dilmurod <101713345+Dilmurodmirzakulov@users.noreply.github.com>
2024-05-16 18:58:38 +0500

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
-    
-})
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"],
+  ["link"],
+  [{ list: "ordered" }, { list: "bullet" }],
+];
+
+$(document).ready(function () {
+  $(".form-card").click(function () {
+    $(".form-card").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  
+  $(".editor").each(function () {
+    const placeholder = $(this).data("placeholder");
+
+    new Quill(this, {
+      placeholder: placeholder,
+      theme: "bubble",
+      modules: {
+        toolbar: toolbarOptions,
+      },
+    });
+  });
+});
